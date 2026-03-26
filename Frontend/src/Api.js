69f42b6e810cb041e1133c587e2c29eeace2a60a
Sahil18th <s.sahilkumar1998@gmail.com>
2026-03-26 13:11:25 +0530

    updated api url for production

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://smartnotes-backend.onrender.com , http://localhost:4000/",                          
   withCredentials: true
 });
 
